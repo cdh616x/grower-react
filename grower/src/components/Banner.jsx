ba415644677c8c1fba3a999b22b6dc38ab7830ec
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-01-12 19:51:42 -0500

    new build
    added banner comp and some css

--- grower/src/components/Banner.jsx
+++ grower/src/components/Banner.jsx
@@ -9,10 +9,15 @@ export default function Banner() {
     <>
      <div className="row">
       <div className="col-lg-7">
-       <img src={logo} />
+       <img src={logo} className="img-fluid" />
+       <h2>Monday - Friday | 10am - 7pm</h2>
+       <h2>Saturday - Sunday | 11am - 4pm</h2>
       </div>
-      <div className="col-lg-5">
-       <h1>Information</h1>
+      <div className="col-lg-5 information">
+       <h1>Give us a call!</h1>
+       <h1>616.862.9095</h1>
+       <h2>4252 14 Mile Rd, Rockford MI</h2>
+       <h2>growersedge1@gmail.com</h2>
       </div>
      </div>
     </>
