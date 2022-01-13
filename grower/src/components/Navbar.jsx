f1d8b0b840c0cfcf334c9a49ea471b3f8500d70d
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-01-12 23:40:47 -0500

    new build
    reformatted and added a font

--- grower/src/components/Navbar.jsx
+++ grower/src/components/Navbar.jsx
@@ -9,16 +9,15 @@ export default function Navbar() {
      <div className="container-fluid">
       <a className="navbar-brand" href="#/">
        <img id="icon" />
-
       </a>
 
        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
        </button>
      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#/">Home</a>
+          <a className="nav-link active" aria-current="page" href="">Home</a>
         </li>
 
         <li className="nav-item dropdown">
@@ -26,12 +25,12 @@ export default function Navbar() {
             Products
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#/ink-gallery">Hardware</a></li>
-            <li><a className="dropdown-item" href="#/paintings">Nutrients</a></li>
+            <li><a className="dropdown-item" href="#">Hardware</a></li>
+            <li><a className="dropdown-item" href="#">Nutrients</a></li>
           </ul>
         </li>
         <li className="nav-item shop-button">
-          <a className="nav-link shop" href="#/shop">Knowledge</a>
+          <a className="nav-link shop" href="#">Knowledge</a>
         </li>
       </ul>
     </div>
