2cca5eb4658c59bd63cbd68efcdc4338790ba676
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-03-14 22:06:43 -0400

    added content to trimming section

--- grower/src/components/gallery-page-trimming.jsx
+++ grower/src/components/gallery-page-trimming.jsx
@@ -4,6 +4,13 @@ import React from "react";
 
 import Navbar from "./Navbar2.jsx";
 import Footer from "./Footer.jsx";
+import EnvItem from "./EnvItem.jsx";
+
+import hydrologo from "../images/logos/hydrofarm.png";
+import gretrim from "../images/hydrofarm/green-trimmer.png";
+import blutrim from "../images/hydrofarm/blue-trimmer.png";
+import trimbag from "../images/hydrofarm/trimbag.png";
+import stackit from "../images/hydrofarm/stack.png";
 
 export default function GalleryPageTrimming(props) {
   return (
@@ -11,6 +18,25 @@ export default function GalleryPageTrimming(props) {
      <Navbar />
 
      <h1 className="page-title">{props.pagetitle}</h1>
+
+     <EnvItem src={hydrologo}
+     alt="Hydrofarm Horticultural Products"
+     href="https://www.hydrofarm.com/search?q=trim%20fast&isc=true&sid=true&cid=0"
+     producttype="Trim Fast Precision Pruners"
+     products1="Anti-Fatigue Lightweight Pruner"
+     products2="Curved Precision Pruner"
+     products3="Bypass Heavy Duty Pruners"
+     producttype2="TrimBag"
+     producttype3="Stack!t"
+     products31="24in & 38in Drying Rack"
+     media_img={gretrim}
+     malt="Trim Fast Precision Pruners"
+     media_img1={blutrim}
+     malt1="Trim Fast Bypass Heavy Duty Pruners"
+     media_img5={trimbag}
+     malt5="TrimBag"
+     media_img6={stackit}
+     malt6="Stack!t Drying Rack" />
 
      <Footer />
     </>
