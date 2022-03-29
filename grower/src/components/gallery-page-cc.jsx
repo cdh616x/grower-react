bafdc02f283a895c05b9242aae64a35b43810c29
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-03-29 11:17:23 -0400

    added more content

--- grower/src/components/gallery-page-cc.jsx
+++ grower/src/components/gallery-page-cc.jsx
@@ -6,12 +6,23 @@ import Footer from "./Footer.jsx";
 
 import ItemUpgraded from "./ItemUpgraded.jsx";
 
+import hydrofarm from "../images/logos/anden.png";
+import anden from "../images/anden/277VAC.png";
+
 export default function CC(props) {
   return (
     <>
     <Navbar />
 
     <h1 className="page-title">{props.pagetitle}</h1>
+
+    <ItemUpgraded src={hydrofarm}
+    href="https://www.anden.com/product_category/dehumidifier/"
+    alt="Hydrofarm Climate Control"
+    producttype="Dehumidifiers"
+    media_img={anden}
+    item="Anden 277VAC Dehumidifier"
+    malt="Anden 277VAC Dehumidifier" />
 
     <Footer />
     </>
--- grower/src/components/gallery-page-water.jsx
+++ grower/src/components/gallery-page-water.jsx
@@ -4,12 +4,67 @@ import React from "react";
 
 import Navbar from "./Navbar2.jsx";
 import Footer from "./Footer.jsx";
+import ItemUpgraded from "./ItemUpgraded.jsx";
+
+import activeAqua from "../images/logos/active-aqua.png";
+import subPump from "../images/active-aqua/pump.png";
+import airPump from "../images/active-aqua/airpump.png";
+
+import hydrologic from "../images/logos/hydrologic.png";
+import smallBoy from "../images/hydrologic/smallboy.png";
+import tallBoy from "../images/hydrologic/tallboy.png";
+import stealthRO from "../images/hydrologic/stealthRO.png";
+import tubing from "../images/hydrologic/tubing.png";
+
+import mondi from "../images/logos/mondi.png";
+import sumpPump from "../images/mondi/sumppump.png";
 
 export default function GalleryPageWater(props) {
   return (
     <>
     <Navbar />
+
      <h1 className="page-title">{props.pagetitle}</h1>
+
+     <ItemUpgraded src={activeAqua}
+     href="http://www.activeaquahydroponics.com/"
+     alt="Active Aqua"
+     producttype="Pumps"
+     media_img={subPump}
+     item="Submersible Pump | 1000 - 160"
+     malt="Active Aqua Sumbersible Pumps"
+     media_img1={airPump}
+     item1="Air Pump | 110 - 7.8"
+     malt1="Active Aqua Air Pumps" />
+
+     <ItemUpgraded src={hydrologic}
+     href="https://www.hydrologicsystems.com/pages/hydroponics-gardening"
+     alt="HydroLogic Purification Systems"
+     producttype="Water Filtration"
+     media_img={smallBoy}
+     item="smallBoy"
+     malt="Hydrologic smallBoy"
+     media_img1={tallBoy}
+     item1="tallBoy"
+     malt1="Hydrologic tallBoy"
+     media_img2={stealthRO}
+     item2="Stealth RO"
+     malt2="Hydrologic Stealth RO"
+     media_img3={tubing}
+     item3="Polyethelene Tubing"
+     malt3="Hydrologic Polyethelene Tubing"
+     otherProducts="Replacement Filters"
+     otherItems="Chlorashield, Deionization Filter, Catalytic Carbon Converter"
+     otherItems1="Stealth RO1000" />
+
+     <ItemUpgraded src={mondi}
+     href="https://mondiproducts.com/product/utility-sump-pump-1585x-gold-series/"
+     alt="Mondi"
+     producttype="Pumps"
+     media_img100={sumpPump}
+     item100="Utility & Sump Pump 1585x - Gold Series"
+     malt100="Mondi Utility & Sump Pump 1585x - Gold Series" />
+
     <Footer />
     </>
   )
